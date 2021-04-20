4470d195b4bbb60223a918392fbd0b92aa1d33a9
Andrii Yurchenko <yurchenkoandrey1631007@gmail.com>
2021-04-20 19:39:34 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Dz 7.py

--- Dz 14.py
+++ Dz 14.py
@@ -1,6 +1,10 @@
-x = [int(y) for y in input().split()]
-k = int(input())
+from random import randint
+lst = [randint(10,40) for _ in range(20)]
+x = lst
+print(lst)
+k = int(input('Введите индекс на удаление: '))
 for i in range(k + 1, len(x)):
     x[i - 1] = x[i]
 x.pop()
 print(' '.join([str(i) for i in x]))
+
