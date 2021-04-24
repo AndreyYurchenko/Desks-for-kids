552f2b5804407551424fc75ccb9baee769772b8f
Andrii Yurchenko <yurchenkoandrey1631007@gmail.com>
2021-04-24 20:38:52 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Dz 7.py

--- Dz 19.py
+++ Dz 19.py
@@ -1,9 +1,9 @@
-from random import randint
-lst1 = [randint(10, 200) for _ in range(20)]
-lst2 = [randint(10, 200) for _ in range(20)]
-print(lst1)
-print(lst2)
-print(len(set(lst1) and set(lst2)))
+# from random import randint
+# lst1 = [randint(10, 200) for _ in range(20)]
+# lst2 = [randint(10, 200) for _ in range(20)]
+# print(lst1)
+# print(lst2)
+# print(len(set(lst1) and set(lst2)))
 
 
 print(len(set(input().split()) and set(input().split())))
