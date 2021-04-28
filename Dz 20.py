da0ea4bd0eb655e5ecee1746b1a3a91432abfbb0
Andrii Yurchenko <yurchenkoandrey1631007@gmail.com>
2021-04-28 13:25:09 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Dz 7.py

--- Dz 20.py
+++ Dz 20.py
@@ -1,7 +1,7 @@
-v =  [[34587,'Learning Python','Mark Lutz',4,40.95],
+v = [[34587,'Learning Python','Mark Lutz',4,40.95],
          [98762,'Programming Python','Mark Lutz',5,56.80],
          [77226,'Head First Python','Paul Barry',3,32.95],
-         [88112,'Einführung in Python3','Bernd Klein',3,24.99]
+         [88112,'Einführung in Python3','Bernd Klein',3,24.99],
         ]
 # y = []
 # for i in v:
@@ -11,4 +11,6 @@ v =  [[34587,'Learning Python','Mark Lutz',4,40.95],
 #        y.append((i[0],i[-1]*i[-2]))
 #
 # print(y)
-print(list(map(lambda i: (i[0], i[-1] * i[-2]) if i[-1] * i[-2] > 100 else i[-1] * i[-2] + 10, v)))
+#print([(i[0], i[-1] * i[-2]) if i [-1] * i [-2] > 100 else (i[0], i[-1] * i[-2]+10) for i in v])
+
+print(list(map(lambda i: [i[0], i[-1] * i[-2]] if i[-1] * i[-2] > 100 else (i[0],i[-1] * i[-2] + 10), v)))
