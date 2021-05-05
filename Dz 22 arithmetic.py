496a794bfa5695b05363fc7a9d95f870c7db9435
Andrii Yurchenko <yurchenkoandrey1631007@gmail.com>
2021-05-05 18:15:24 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Dz 7.py

--- Dz 22 arithmetic.py
+++ Dz 22 arithmetic.py
@@ -11,11 +11,8 @@ def arithmetic(x, y, z):
 		return 'Invalid operation'
 
 
-
 print(arithmetic(5, 2, '+'))
 print(arithmetic(5, 2, '*'))
 print(arithmetic(5, 2, '-'))
 print(arithmetic(5, 2, '/'))
 print(arithmetic(5, 2, 0))
-
-
