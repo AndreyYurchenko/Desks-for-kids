7dd04f6c023be0cc7847830e131e1d6251a11f38
Andrii Yurchenko <yurchenkoandrey1631007@gmail.com>
2021-05-18 22:53:43 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Dz 7.py

--- Dz 28.py
+++ Dz 28.py
@@ -1,6 +1,8 @@
+# Это фиаско ничего не работает как и мой мозг сейчас.Где-то я повернул не туда,а времени с нуля делать нет.
 file = open('src_14.txt', encoding='utf-8')
 counter = 0
 summa = 0
+lst = []
 for line in file:
     counter += 1
     x = len(line)
@@ -10,9 +12,40 @@ for line in file:
             summa += num
             k = summa / num
             f = line.find(' ')
+            j = '{:} {:18} {:<10} {:^30}'.format('Ученик двоечник:', line[f] + line[f: 20], line[0].capitalize() + '.', '%.2f' % k)
+            y = '{:1} {:<1} {:>10}'.format(line[f] + line[f: 20], line[0].capitalize() + '.', '%.2f' % k)
             if k < 5:
-                print('{} {:18} {:19} {:20}'.format('Ученик двоечник:', line[f] + line[f: 20], line[0].capitalize() + '.', '%.2f' % k))
+                lst.append(y.strip('\n'))
+                print(j)
+
+            else:
+                lst.append(y.strip('\n'))
             break
 
 y = summa / counter
 print('Средний бал по классу', y)
+# print(lst)
+# for f in lst:
+#     print('{:1}'.format(f))
+file = open('src_14.txt', encoding='utf-8')
+counter = 0
+summa = 0
+lst = []
+for line in file:
+    counter += 1
+    x = len(line)
+    for i in range(x):
+        if line[i].isdigit():
+            num = int(line[i])
+            summa += num
+            k = summa / counter
+            f = line.find(' ')
+            if k != 0:
+                print('{:18} {:19} {:20}'.format(line[f] + line[f: 20], line[0].capitalize() + '.', '%.2f' % k))
+            break
+file.close()
+
+
+
+
+
