2fb7df2d39667efb5f5b720340303119a9f94881
Andrii Yurchenko <81506757+AndreyYurchenko@users.noreply.github.com>
2021-11-20 22:33:36 +0200

    Lf

---
+++ php/demo.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale-1">
+   <titile>position</title>
+   <link rel="stylesheet" href="listblocks.css">
+</head>
+<body>
+  <div class="perent">
+    Родитель
+    <div class ="block block1">
+      block1
+    </div>
+  </div>
+</body>
+</html>
+
