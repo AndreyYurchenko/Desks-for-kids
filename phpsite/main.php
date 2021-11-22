0b8a952a12ad015dcb30cb21f1143bc00f98b344
Andrii Yurchenko <81506757+AndreyYurchenko@users.noreply.github.com>
2021-11-22 18:33:14 +0200

    Yjd

--- phpsite/main.php
+++ phpsite/main.php
@@ -4,17 +4,12 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale-1">
-   <titile>position</title>
-   <link rel="stylesheet" href="listblocks.css">
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+   <title>БцМаршал<title>
 </head>
 <body>
-<div class="wrapper">
-  <div class="perent">
-    Родитель
-    <div class ="block block1">
-      block1
-    </div>
-  </div>
+
 </body>
 </html>
 
