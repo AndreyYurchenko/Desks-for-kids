c45959b0071241900c20fe0144ee09bff0858b31
Andrii Yurchenko <81506757+AndreyYurchenko@users.noreply.github.com>
2021-11-30 20:09:35 +0200

    hg

--- phpsite/mangaregistretion.php
+++ phpsite/mangaregistretion.php
@@ -4,11 +4,12 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale-1">
-   <meta http-equiv="X-UAComatible" content="ie=edge">
-   <title>Форма регистрации<title>
+   <link rel="stylesheet" href="style.css">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+   <title> Форма регистрации <title>
 </head>
 <body>
-    <h1>Форма регистрации</h1>
-    
+    <h1> Форма регистрации </h1>
+
 </body>
 </html>
